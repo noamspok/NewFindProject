def1babba8fd56dd44d4fbf5956d7eff44dd2179
noamspok <noamspok@gmail.com>
2017-12-17 15:19:17 +0200

    made some progress

--- FinedProjectApp/Scripts/StudentScript.js
+++ FinedProjectApp/Scripts/StudentScript.js
@@ -1,26 +1,37 @@
-﻿$("#btn_submit").click(function () {
-    var student = {
-        userName: $("#userName").val(),
-        Password: $("#Password").val(),
-        Email: $("#Email").val(),
-        JoinDate: 0,
-        Victories: 0,
-        Losses: 0
-    };
-    if (user.userName === "") {
-        alert("user name is messing")
-    }
-    else if (user.Password === "") {
-        alert("password is messing")
-    }
-    else if (user.Email === "") {
-        alert("password is messing")
-    } else {
-        var userUrl = "../api/Users";
-        $.post(userUrl, user).done(function () {
-            alert("User added successfully");
-            sessionStorage.setItem("userName", user.userName);
-            location.replace("../View/HomePage.html");
-        });
-    }
-});
+﻿
+
+
+
+ko.validation.registerExtenders();
+
+function AppViewModel() {
+    var that = this;
+    this.UserName = ko.observable("").extend({ required: { message: "אנא מלא/י את שם המשתמש שלך" } });
+    this.IdNum = ko.observable("").extend({ required: { message: "אנא מלא/י את מס' תעודת הזהות שלך" }, number: true });
+    this.PName = ko.observable("").extend({ required: { message: "אנא מלא/י את השם הפרטי שלך" } });
+    this.FName = ko.observable("").extend({ required: { message: "אנא מלא/י את שם המשפחה שלך" } });
+    this.Avg = ko.observable("").extend({ required: { message: "אנא מלא/י את מס' תעודת הזהות שלך" }, number: true });
+    this.GenderOptions = ko.observableArray(["זכר", "נקבה"]);
+    this.GroupOptions = ko.observableArray(["1", "2", "3", "4"]);
+    this.ExpOptions = ko.observableArray(["ללא נסיון", "עד שנה", "1-2 שנים", "3-5 שנים","יותר מ-5 שנים"]);
+    this.BirthOptions = ko.observableArray(["1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002"]);
+    this.selectedBirth = ko.observable("");
+    this.selectedGender = ko.observable("");
+    this.selectedGroup = ko.observable("");
+    this.selecteExp = ko.observable("");
+    this.courses = ko.observableArray([""]);
+    this.codeLang = ko.observableArray([""]);
+    
+        this.StartBtn = function () {
+           
+        };
+    
+
+    this.isFormValid = ko.computed(function () {
+
+        return  this.UserName.isValid();
+    }, this);
+}
+
+ko.applyBindings(AppViewModel);
+
