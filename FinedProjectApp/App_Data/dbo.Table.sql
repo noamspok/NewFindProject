69d23b3a9929938429edd2157ab11c791abddc12
maorSeri <maorroam14@gmail.com>
2018-10-18 19:09:14 +0300

    עם שאלון סיום

---
+++ FinedProjectApp/App_Data/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Survey]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Qa] INT NULL, 
+    [Qb] INT NULL, 
+    [Qc] INT NULL, 
+    [Qd] INT NULL, 
+    [Qe] INT NULL
+)
---
+++ FinedProjectApp/Controllers/SurveyController.cs
@@ -0,0 +1,45 @@
+﻿using FinedProjectApp.Models.SampleProjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FinedProjectApp.Controllers
+{
+    public class SurveyController : ApiController
+    {
+        // GET: api/Survey
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Survey/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Survey
+        public void Post(SurveyResult surveyResult)
+        {
+            if (!Moderators.ModSurvey.UpdateTables(surveyResult))
+            {
+
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // PUT: api/Survey/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Survey/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
